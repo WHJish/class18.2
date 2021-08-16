248c4a9ae7b5882052dd70640c2d0ad739b7c14b
WHJish <88065313+WHJish@users.noreply.github.com>
2021-08-16 16:52:41 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var s1,s2,s3;
+function setup() {
+  createCanvas(400,400);
+  s1 = new Student (" Ishan",12,7);
+  s2 = new Student ("idk",17,12);
+  s1.display()
+s2.display()
+}
+
+function draw() 
+{
+  background(51);
+  
+}
